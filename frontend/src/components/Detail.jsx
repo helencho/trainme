05479e32a682c15926fa6f98819c8c8e8800c795
helencho <helencho@ac.c4q.nyc>
2018-03-03 11:41:38 -0500

    finish setting up component skeleton

---
+++ frontend/src/components/Detail.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Detail extends Component {
+    render() {
+        return (
+            <h1>Detailed description on job</h1>
+        )
+    }
+}
+
+export default Detail 
