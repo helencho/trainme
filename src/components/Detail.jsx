c25b342543c10444b27f2f699dac7efc4b1389cd
helencho <helencho@ac.c4q.nyc>
2018-03-04 17:00:08 -0500

    add banner to homepage

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -81,35 +81,45 @@ class Detail extends Component {
                     <i class="fas fa-times" onClick={this.props.handleBack}></i>
                 </span>
 
-                <h2>{course.course_name || 'N/A'}</h2>
-                <div className='detail-name'>{course.organization_name || 'N/A'}</div>
-                <div className='detail-address'>{course.address1 || 'N/A'} {course.city || 'N/A'} {course.zip_code || 'N/A'} {course.borough || 'N/A'}</div>
+                <div className='detail-section'>
+                    <h2>{course.course_name || 'N/A'}</h2>
+                    <p className='detail-name'>{course.organization_name || 'N/A'}</p>
+                    <p className='detail-address'>{course.address1 || 'N/A'} {course.city || 'N/A'} {course.zip_code || 'N/A'} {course.borough || 'N/A'}</p>
+                </div>
                 <hr />
-                <div className='detail-contact'>Contact: {course.contact_firstname || 'N/A'} {course.contact_lastname || 'N/A'}</div>
-
-                <div className='detail-phone'>Phone: {course.phone1 || 'N/A'}</div>
-                <div className='detail-phone'>Fax: {course.fax || 'N/A'}</div><br />
-
-                <div className='detail-web'>Website: {course.website ? <a href={course.website} target='_blank'>{course.website}</a> : 'n/a'}</div>
+                <div className='detail-section detail-section-column'>
+                    <div>
+                        <p className='detail-contact'>Contact: {course.contact_firstname || 'N/A'} {course.contact_lastname || 'N/A'}</p>
+                        <p className='detail-phone'>Phone: {course.phone1 || 'N/A'}</p>
+                        <p className='detail-phone'>Fax: {course.fax || 'N/A'}</p>
+                        <p className='detail-web'>Website: {course.website ? <a href={course.website} target='_blank'>{course.website}</a> : 'n/a'}</p>
+                    </div>
+                    <div>
+                        <p className='detail-type'>Training Type: {course.delivery_method || 'N/A'}</p>
+                        <p className='detail-instructor'>Instructor Credentials: {course.instructor_credentials || 'N/A'}</p>
+                        <p className='detail-class'>Max Class Size: {course.max_class_size || 'N/A'}</p>
+                        <p className='detail-class'>HRA Approved: {course.is_hra || 'N/A'}</p>
+                    </div>
+                </div>
                 <hr />
-                <div className='detail-type'>Training Type: {course.delivery_method || 'N/A'}</div>
-                <div className='detail-instructor'>Instructor Credentials: {course.instructor_credentials || 'N/A'}</div>
-                <div className='detail-class'>Max Class Size: {course.max_class_size || 'N/A'}</div>
-                <div className='detail-class'>HRA Approved: {course.is_hra || 'N/A'}</div>
+                <div className='detail-section'>
+                    <p className='detail-description'>About This Course</p>
+                    <p>{course.coursedescription || 'N/A'}</p>
+                </div>
                 <hr />
-                <div className='detail-description'>About This Course: {course.coursedescription || 'N/A'}</div>
+                {/* <p className='detail-schedule' >Schedule: {course.schedule || 'N/A'}</p> */}
+                <div className='detail-section'>
+                    <p className='detail-duration'>Duration: {course.duration || 'N/A'} {course.duration_unit.toLowerCase() || 'N/A'} </p>
+                    <p className='detail-placement'>Job placement is {course.job_placement_services ? 'available' : 'not available'}</p>
+                    <p className='detail-services'>Financial aid is {course.financial_aid_services ? 'offered' : 'not offered'}</p>
+                </div>
                 <hr />
-                {/* <div className='detail-schedule' >Schedule: {course.schedule || 'N/A'}</div> */}
-                <div className='detail-duration'>Duration: {course.duration || 'N/A'} {course.duration_unit || 'N/A'} </div>
-
-                <div className='detail-placement'>Job placement is {course.job_placement_services ? 'available' : 'not available'}</div>
-                <div className='detail-services'>Financial aid is {course.financial_aid_services ? 'offered' : 'not offered'}</div>
-
-                <hr />
-                <div className='detail-total'>Cost Total: ${course.cost_total || 'N/A'}</div>
-                <div className='detail-includes'>Cost Includes: {course.cost_includes}</div>
-                {/* <div className='detail-not-include'>Cost Does Not Include: {course.cost_does_not_include || 'N/A'}</div> */}
-                <div className='detail-prereq'>Prerequisites: {course.prerequisites || 'N/A'}</div>
+                <div className='detail-section'>
+                    <p className='detail-total'>Cost Total: ${course.cost_total || 'N/A'}</p>
+                    <p className='detail-includes'>Cost Includes: {course.cost_includes}</p>
+                    {/* <p className='detail-not-include'>Cost Does Not Include: {course.cost_does_not_include || 'N/A'}</p> */}
+                    <p className='detail-prereq'>Prerequisites: {course.prerequisites || 'N/A'}</p>
+                </div>
                 <hr />
 
                 {isSaved
