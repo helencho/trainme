42e2ec65fc1cfd3b275aea17103f7b4c66634e0e
helencho <helencho@ac.c4q.nyc>
2018-03-04 19:04:02 -0500

    responsive detail page

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -81,24 +81,17 @@ class Detail extends Component {
                     <i class="fas fa-times" onClick={this.props.handleBack}></i>
                 </span>
 
-                <div className='detail-section'>
-                    <h2>{course.course_name || 'N/A'}</h2>
-                    <p className='detail-name'>{course.organization_name || 'N/A'}</p>
-                    <p className='detail-address'>{course.address1 || 'N/A'} {course.city || 'N/A'} {course.zip_code || 'N/A'} {course.borough || 'N/A'}</p>
-                </div>
-                <hr />
                 <div className='detail-section detail-section-column'>
+                    <div>
+                        <h2>{course.course_name || 'N/A'}</h2>
+                        <p className='detail-name'>{course.organization_name || 'N/A'}</p>
+                        <p className='detail-address'>{course.address1 || 'N/A'} {course.city || 'N/A'} {course.zip_code || 'N/A'} {course.borough || 'N/A'}</p>
+                    </div>
                     <div>
                         <p className='detail-contact'>Contact: {course.contact_firstname || 'N/A'} {course.contact_lastname || 'N/A'}</p>
                         <p className='detail-phone'>Phone: {course.phone1 || 'N/A'}</p>
-                        <p className='detail-phone'>Fax: {course.fax || 'N/A'}</p>
+                        {/* <p className='detail-phone'>Fax: {course.fax || 'N/A'}</p> */}
                         <p className='detail-web'>Website: {course.website ? <a href={course.website} target='_blank'>{course.website}</a> : 'n/a'}</p>
-                    </div>
-                    <div>
-                        <p className='detail-duration'>Duration: {course.duration || 'N/A'} {course.duration_unit.toLowerCase() || 'N/A'} </p>
-                        <p className='detail-type'>Training Type: {course.delivery_method || 'N/A'}</p>
-                        <p className='detail-instructor'>Instructor Credentials: {course.instructor_credentials || 'N/A'}</p>
-                        <p className='detail-class'>Max Class Size: {course.max_class_size || 'N/A'}</p>
                     </div>
                 </div>
                 <hr />
@@ -108,17 +101,25 @@ class Detail extends Component {
                 </div>
                 <hr />
                 {/* <p className='detail-schedule' >Schedule: {course.schedule || 'N/A'}</p> */}
-                <div className='detail-section'>
-                    <p className='detail-class'>HRA Approved: {course.is_hra || 'N/A'}</p>
-                    <p className='detail-placement'>Job Placement: {course.job_placement_services ? 'Available' : 'Not available'}</p>
-                    <p className='detail-services'>Financial Aid: {course.financial_aid_services ? 'Offered' : 'Not offered'}</p>
+                <div className='detail-section detail-section-column'>
+                    <div>
+                        <p className='detail-duration'>Duration: {course.duration || 'N/A'} {course.duration_unit.toLowerCase() || 'N/A'} </p>
+                        <p className='detail-type'>Training Type: {course.delivery_method || 'N/A'}</p>
+                        <p className='detail-instructor'>Instructor Credentials: {course.instructor_credentials || 'N/A'}</p>
+                        <p className='detail-class'>Max Class Size: {course.max_class_size || 'N/A'}</p>
+                    </div>
+                    <div>
+                        <p className='detail-class'>HRA Approved: {course.is_hra || 'N/A'}</p>
+                        <p className='detail-placement'>Job Placement: {course.job_placement_services ? 'Available' : 'Not available'}</p>
+                        <p className='detail-services'>Financial Aid: {course.financial_aid_services ? 'Offered' : 'Not offered'}</p>
+                        <p className='detail-prereq'>Prerequisites: {course.prerequisites || 'None'}</p>
+                    </div>
                 </div>
                 <hr />
                 <div className='detail-section'>
                     <p className='detail-total'>Costs: ${course.cost_total || 'N/A'}</p>
                     <p className='detail-includes'>Cost Includes: {course.cost_includes}</p>
                     {/* <p className='detail-not-include'>Cost Does Not Include: {course.cost_does_not_include || 'N/A'}</p> */}
-                    <p className='detail-prereq'>Prerequisites: {course.prerequisites || 'N/A'}</p>
                 </div>
                 <hr />
 
